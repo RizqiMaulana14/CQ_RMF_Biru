bae2a57a185ad5ac435c1f8d53eba1a335e1d6e0
RizqiMaulana14 <maulanafitrah14@gmail.com>
2026-03-13 16:25:02 +0700

    Membuat Koneksi Database

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +15,19 @@ namespace PraktikumADO
 {
     public partial class Form1 : Form
     {
+        SqlConnection conn;
+        SqlCommand cmd;
         public Form1()
         {
             InitializeComponent();
         }
+        private void Koneksi()
+        {
+            conn = new SqlConnection(
+                "Data Source =RIZQI\\RIZQIMAULANA;Initial Catalog=DBAkademikADO;Integrated Security=True"
+            );
+        }
+        
     }
 }
+
