bb7af3cb3f0ed696f06584951c244998bb4079e8
RizqiMaulana14 <maulanafitrah14@gmail.com>
2026-03-13 16:35:16 +0700

    Latihan 1. Hitung Dosen

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -92,6 +92,23 @@ namespace PraktikumADO
                 MessageBox.Show(ex.Message);
             }
         }
+        private void btnHitungDosen_click(object sender, EventArgs e)
+        {
+            try
+            {
+                Koneksi();
+                conn.Open();
+                string query = "SELECT COUNT(*) FROM Dosen";
+                cmd = new SqlCommand(query, conn);
+                int jumlah = (int)cmd.ExecuteScalar();
+                txtHasil.Text = jumlah.ToString();
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
         
     }
 }
