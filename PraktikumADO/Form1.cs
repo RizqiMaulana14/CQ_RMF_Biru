889ef6c4dc89a42adf85ac2378113fec864f4353
RizqiMaulana14 <maulanafitrah14@gmail.com>
2026-03-13 16:37:39 +0700

    Latihan 3. Tambah ProgramStudi

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -126,7 +126,23 @@ namespace PraktikumADO
                 MessageBox.Show(ex.Message);
             }
         }
-        
+        private void btnTambah_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Koneksi();
+                conn.Open();
+                string query = "INSERT INTO ProgramStudi VALUES ('MI01','Manajemen Informatika')";
+                cmd = new SqlCommand(query, conn);
+                int hasil = cmd.ExecuteNonQuery();
+                MessageBox.Show("Jumlah baris terpengaruh : " + hasil);
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
     }
 }
 
